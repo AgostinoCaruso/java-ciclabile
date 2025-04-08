3ba497a45aaf4a9fd99bc4d6962ed5f7c7103369
AgostinoCaruso <agostinocaruso321@gmail.com>
2025-04-08 15:11:27 +0200

    add bozze

--- src/org/lessons/java/JavaCiclabile.java
+++ src/org/lessons/java/JavaCiclabile.java
@@ -2,13 +2,41 @@ package org.lessons.java;
 
 public class JavaCiclabile {
 
-    private int numeroCiclo = 0;
-    private int[] arrayIntero = {1,2,3,4};
+    private int[] arrayIntero = { 1, 2, 3, 4 };
+    private int numeroCicloIterato = arrayIntero.length;
 
-    JavaCiclabile(){
-        //qui dentro deve starci una funzione che inizializzi un array
+    JavaCiclabile() {
+        addElemento();
+        // qui dentro deve starci una funzione che inizializzi un array
     }
-    JavaCiclabile(int[] array){
+
+    JavaCiclabile(int[] array) {
         array = arrayIntero;
+    }
+
+    // get&set
+    public int[] GetArrayIntero() {
+        return arrayIntero;
+    }
+
+    public int GetNumeroIterato() {
+        return numeroCicloIterato;
+    }
+
+    // metodi
+    public int GetElementoSuccessivo(int[] arrayIntero) {
+        // deve tornare l elemento succesivo dell'array, tenendo conto dell'inidice ad
+        // ogni invocazione
+        return 0;
+    }
+
+    public boolean HasAncoraElementi(int numeroCicloIterato, int[] arrayIntero) {
+        if (numeroCicloIterato > arrayIntero.length)
+            return false;
+        return true;
+    }
+
+    public void addElemento() {
+        
     }
 }
