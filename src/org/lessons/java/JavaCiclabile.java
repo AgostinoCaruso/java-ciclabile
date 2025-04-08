728ca89ab39d681c37d4cf46e20d1043f494a728
AgostinoCaruso <agostinocaruso321@gmail.com>
2025-04-08 15:04:43 +0200

    first commit

---
+++ src/org/lessons/java/JavaCiclabile.java
@@ -0,0 +1,14 @@
+package org.lessons.java;
+
+public class JavaCiclabile {
+
+    private int numeroCiclo = 0;
+    private int[] arrayIntero = {1,2,3,4};
+
+    JavaCiclabile(){
+        //qui dentro deve starci una funzione che inizializzi un array
+    }
+    JavaCiclabile(int[] array){
+        array = arrayIntero;
+    }
+}
